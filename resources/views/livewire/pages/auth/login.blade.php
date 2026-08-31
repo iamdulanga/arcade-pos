867f368652600eeb0a8a776e2c0956507c452b0f
Dulanga Dasanayake <dulanganiroshan721@gmail.com>
2026-08-31 16:19:07 +0530

    loading state (spinner) removed

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -68,24 +68,13 @@ new #[Layout('layouts.guest')]
             @endif
         </div>
 
-        <!-- Submit Button with Loading State -->
+        <!-- Submit Button without Spinner -->
         <div class="pt-2">
             <x-primary-button class="w-full justify-center py-2.5" wire:loading.attr="disabled">
-                <!-- Default Text -->
                 <span wire:loading.remove wire:target="login">
                     {{ __('Sign in') }}
                 </span>
-
-                <!-- Loading State -->
-                <span wire:loading wire:target="login" class="inline-flex items-center">
-                    <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
-                        </circle>
-                        <path class="opacity-75" fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                        </path>
-                    </svg>
+                <span wire:loading wire:target="login">
                     {{ __('Signing in...') }}
                 </span>
             </x-primary-button>
